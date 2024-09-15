b6abf4a8c9f64b906627b7f9247a43d66c9a81a9
vvc-git <victorvcunha@hotmail.com>
2024-09-15 19:41:15 -0300

    settings file

---
+++ first-spring-app/src/main/java/com/vvc/first_spring_app/FirstSpringAppApplication.java
@@ -0,0 +1,15 @@
+package com.vvc.first_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+@Profile("dev")
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
